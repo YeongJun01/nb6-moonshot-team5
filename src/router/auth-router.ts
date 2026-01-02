a2c6045e89bdd8b3ecaa679af6defc0ea94de275
Yeongjun <ing08114@gmail.com>
2026-01-02 09:37:35 +0900

    [생성] 인증, 로그인, 토큰 재발급 기능

---
+++ src/router/auth-router.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { asyncHandler } from '../middleware/handlerFn';
+import authController from '../controller/auth-controller';
+import { authenticateRefresh } from '../middleware/authenticate';
+
+const authRouter = express.Router();
+
+authRouter
+  .post('/register', asyncHandler(authController.register))
+  .post('/login', asyncHandler(authController.login))
+  .post('/refresh', authenticateRefresh, asyncHandler(authController.refreshToken))
+  .post('/logout', asyncHandler(authController.logout));
+
+export default authRouter;
